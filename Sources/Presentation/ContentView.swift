bc732f50776d578e4e19a546dddd3258dbf5b1a7
Tianyue Qi <taylor_chyi@icloud.com>
2025-07-07 19:43:14 +0800

    feat: add user settings module

--- Sources/Presentation/ContentView.swift
+++ Sources/Presentation/ContentView.swift
@@ -1,17 +1,41 @@
 import SwiftUI
+import Domain
+#if canImport(Infrastructure)
+import Infrastructure
+#endif
 
 public struct ContentView: View {
-    public init() {}
+    private let settingsRepository: UserSettingsRepository
+    @State private var fontSize: Double
+
+    public init(settingsRepository: UserSettingsRepository) {
+        self.settingsRepository = settingsRepository
+        self._fontSize = State(initialValue: settingsRepository.loadSettings().fontSize)
+    }
+
     public var body: some View {
-        Text("GlowBoard Placeholder")
+        VStack {
+            Text("Sample Script")
+                .font(.system(size: fontSize))
+                .padding()
+
+            Slider(value: $fontSize, in: 12...72, step: 1) {
+                Text("Font Size")
+            }
+            .onChange(of: fontSize) { newValue in
+                var settings = settingsRepository.loadSettings()
+                settings.fontSize = newValue
+                settingsRepository.save(settings: settings)
+            }
             .padding()
+        }
     }
 }
 
-#if DEBUG
+#if canImport(Infrastructure) && DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(settingsRepository: InMemoryUserSettingsRepository())
     }
 }
 #endif
